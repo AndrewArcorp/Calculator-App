fe121db186e4ac829eaf5ce80bb2e12549d9741c
AndrewArcorp <andrew.maher@egyptianservicecard.com>
2023-12-26 13:18:20 +0200

    Initial Project

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+    // Implement Subtract
+//    publinc
+
+
+    // Implement Add Function
+//    public static
+}
